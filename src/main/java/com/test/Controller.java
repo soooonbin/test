74621ea8c4435206fa1f443bb8c4241426bc8ece
soooonbin <dlvtoekf4@naver.com>
2025-02-20 18:26:55 +0900

    first commit

---
+++ src/main/java/com/test/Controller.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "test";
+    }
+}
